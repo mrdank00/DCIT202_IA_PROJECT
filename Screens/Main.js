79d5f074384e51a4dd800a374fa23f15d5db1009
mrdank00 <dkkissi001@st.ug.edu.gh>
2021-11-15 23:23:18 +0000

    Added Redux Reducers

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,24 +1,34 @@
 import React, {useEffect, useState} from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { Entypo } from '@expo/vector-icons';
 import axios from "axios";
 
+import Product from "../components/Product";
+
 export function Main({navigation}) {
 
-  const [ , setProducts] = useState();
+  const [ products, setProducts] = useState();
 
 useEffect(() => {
-  axios.get("https://fakestoreapi.com/products").then(data => console.log(data)).catch(err => alert(err.message))
+  axios.get("https://fakestoreapi.com/products").then(data => setProducts(data.data)).catch(err => alert(err.message))
 }, [])
 
     return (
     <View style={{flex:1}}>
-          <View style={{flexDirection:'row', flexBasis:'20%',justifyContent:"space-between",alignItems:'center', paddingHorizontal:20}}>
-            <Text style={{fontWeight:'bold', fontSize: 18}}>Products</Text>
+          <View style={{flexDirection:'row', flexBasis:'15%',justifyContent:"space-between",alignItems:'center', paddingHorizontal:20}}>
+            <Text style={{fontWeight:'bold', fontSize: 18,color:'black'}}>Products</Text>
             <TouchableOpacity  style={{}}> 
               <Entypo name="shopping-cart" size={24} color="black" />
             </TouchableOpacity>
-          </View>
+            <View>
+
+            </View>
+          </View> 
+          <ScrollView style={{flexDirection:'row'}}>
+          {products && products.map(({id, title, price, image}) => (
+            <Product key={id} title={title} price={price} image={image}/>
+          ))}
+          </ScrollView>
       </View>
     );  
 }
@@ -30,4 +40,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cearchcontainer:{
+    height:50,
+    backgroundColor:'grey',
+    borderRadius:10,
+    flex:1,
+    flexDirection:'row'
+    
+  }
 });
